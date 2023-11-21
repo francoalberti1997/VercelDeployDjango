fbdaca7695395d7fa5f53d2569239c5900d54caf
FrancoAlberti1997 <albertifranco1997@hotmail.com>
2023-11-21 02:23:00 -0300

    commimi

---
+++ VercelDjango/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for VercelDjango project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VercelDjango.settings')
+
+application = get_wsgi_application()
